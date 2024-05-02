a0ac27f5b24668e4bfca8089de6869e683ae0eb8
Pavel Bely <paviel.biely@gmail.com>
2024-05-02 22:04:18 +0300

    Fix dirname

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+import { URL } from 'node:url';
 import express from 'express';
 import { createCanvas, loadImage } from 'canvas';
+import path from 'path';
 // import * as fs from 'fs';
+
+const __dirname = new URL('.', import.meta.url).pathname;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +18,8 @@ app.get('/create-pdf', async (req, res) => {
   const canvas = createCanvas(800, 600, 'pdf');
   const ctx = canvas.getContext('2d');
 
-  const backgroundImage = await loadImage('assets/bg.png');
+  const imagePath = path.join(__dirname, '/assets/bg.png');
+  const backgroundImage = await loadImage(imagePath);
   ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
   ctx.font = '40px Arial';
