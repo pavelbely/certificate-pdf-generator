14cc195d3115b0c71f8ebcfb4dfe7be520762c32
Martin <moothreene@gmail.com>
2024-05-03 16:30:55 +0300

    changed chapter properties

--- Chapter.js
+++ Chapter.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const ChapterSchema = new mongoose.Schema({
-  nameRu: { type: String, required: true, min: 2 },
-  nameHe: { type: String, required: false, min: 2 },
+  id: { type: Number, required: true },
+  nameRu: { type: String, min: 2 },
+  nameHe: { type: String, min: 2 },
+  buyer: {
+    nameRu: { type: String, required: false, min: 2 },
+    nameHe: { type: String, required: false, min: 2 },
+  },
 });
 
 const Chapter = mongoose.model("Chapter", ChapterSchema, "chapters");
