eef06380148f3a8fe5b14c6834f0a6490f8ec688
Martin <moothreene@gmail.com>
2024-05-03 17:37:34 +0300

    utility functions for buyer name validation

---
+++ utils.js
@@ -0,0 +1,52 @@
+import Chapter from './Chapter.js';
+
+/*
+Validate the buyer's name
+parameters: chapterId - the id of the chapter in db
+            nameRu - the buyer's name in Russian (optional)
+            nameHe - the buyer's name in Hebrew (optional)
+returns: the buyer's name in hebrew and russian
+*/
+const validateChapterBuyer = async (chapterId, nameRu, nameHe) => {
+  const chapterDoc = await Chapter.findOne({ id: chapterId });
+  console.log(nameRu, nameHe);
+  // If the buyer's name is provided in the query try updating it in the database
+  if (nameRu || nameHe) {
+    // TODO implement translation or an equivalent
+    return await updateBuyer(chapterDoc, nameRu, nameHe);
+  } else {
+    return await getBuyer(chapterDoc);
+  }
+};
+
+
+/*
+Update buyer in the database
+parameters: chapter - mongoose document of the chapter
+            nameRu - the buyer's name in Russian
+            nameHe - the buyer's name in Hebrew
+returns: updated buyer field from the database or throws an error if the buyer already exists
+*/
+const updateBuyer = async (chapter, nameRu, nameHe) => {
+  console.log(chapter.buyer);
+  // If the buyer's name is not provided in the database, update it
+  if (!(chapter.buyer?.nameRu || chapter.buyer?.nameHe)) {
+    chapter.buyer = {
+      nameRu: nameRu,
+      nameHe: nameHe,
+    };
+    await chapter.save();
+    return chapter.buyer;
+    // If the buyer's name is already provided in the database, throw an error
+  } else {
+    console.log('exists');
+    throw new Error('Buyer already exists');
+  }
+};
+
+//If the buyer's name is not provided in the query, get it from the database
+const getBuyer = async (chapter) => {
+  return chapter.buyer;
+};
+
+export default validateChapterBuyer;
