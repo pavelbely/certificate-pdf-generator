90352f62d4fb12ac5c9993b7261a7f176d578bc0
Martin <moothreene@gmail.com>
2024-05-15 12:05:05 +0300

    init

---
+++ scripts/csv_to_json.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import Papa from 'papaparse';
+const csv_to_convert = fs.createReadStream('./scripts/csv_to_convert.csv');
+Papa.parse(csv_to_convert, {
+  header: true,
+  complete: (result) => {
+    let price_data = result.data.map(({ID,Price})=>({ID,Price}));
+    fs.writeFileSync('./scripts/csv_converted.json', JSON.stringify(price_data));
+  }
+});
